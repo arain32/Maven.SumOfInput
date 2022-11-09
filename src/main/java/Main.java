b59381a015c86502145cf1e6d450aed597ad12e6
Adrienne <adrienne@Zipcoders-MacBook-Pro-101.local>
2022-11-08 21:50:02 -0500

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,24 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number");
+        int userinput= scanner.nextInt();
+        //System.out.println("your number is " + userinput);
+         int sum = sumOfNumbers(userinput);
+        System.out.println("sum of your input is " + sum);
     }
     
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i= 0; i <= n; i++){
+            //System.out.println("the number i is "+ i);
+         sum = sum + i;
+            //System.out.println("sum is " + sum);
+
+
+        }
+
+        return sum;
     }
     
     // for Extra Credit
