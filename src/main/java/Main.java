bc6f897c9aeda7541b27c3b612a97eaba77cda89
Adrienne <adrienne@Zipcoders-MacBook-Pro-101.local>
2022-11-08 21:12:54 -0500

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
     }
     
     int sumOfNumbers(int n) {
